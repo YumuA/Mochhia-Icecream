8db1d601847df521c7989515a2c7ff374cea5867
YumuA <bons.santi@hotmail.com>
2023-05-14 20:54:12 -0600

    Eliminar error d

--- Heladería_pruebas-copy/vale.java
+++ Heladería_pruebas-copy/vale.java
@@ -1,9 +1,13 @@
-public class vale
-{
-   String usuario;
-   String producto1;
-   String producto2;
-   double precio1;
-   double precio2;
-   double promociones;
+public class Vale {
+    public int heladoSeleccionado;
+    public int cantidad;
+    public double precioUnitario;
+    public double total;
+
+    public Vale(int heladoSeleccionado, int cantidad, double precioUnitario, double total) {
+        this.heladoSeleccionado = heladoSeleccionado;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.total = total;
+    }
 }
